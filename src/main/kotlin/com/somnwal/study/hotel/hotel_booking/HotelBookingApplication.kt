aca2f8e4972ebbe74876da42473adbae11f360d8
솜놜 <90139018+somnwal@users.noreply.github.com>
2025-05-01 21:08:27 +0900

    Initial commit

---
+++ src/main/kotlin/com/somnwal/study/hotel/hotel_booking/HotelBookingApplication.kt
@@ -0,0 +1,11 @@
+package com.somnwal.study.hotel.hotel_booking
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class HotelBookingApplication
+
+fun main(args: Array<String>) {
+	runApplication<HotelBookingApplication>(*args)
+}
---
+++ src/test/kotlin/com/somnwal/study/hotel/hotel_booking/HotelBookingApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.somnwal.study.hotel.hotel_booking
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class HotelBookingApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
